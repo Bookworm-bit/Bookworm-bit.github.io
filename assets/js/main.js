ff841f0577c97a06f17709a80bc5779a57a75059
Bookworm-bit <81590955+Bookworm-bit@users.noreply.github.com>
2022-11-19 11:02:00 -0500

    e

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
-var gamePiece;
+var player;
+var door;
 
 function startGame() {
-    gamePiece = new component(30, 30, "red", 10, 120);
+    player = new component(30, 30, "red", 10, 120);
+    door = new component(30, 10, "blue", 200, 200);
     gameArea.start();
 }
 
@@ -13,6 +15,12 @@ var gameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function(e) {
+            gameArea.key = e.key;
+        })
+        window.addEventListener('keydown', function(e) {
+            gameArea.key = false;
+        })
     },
     clear: function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -24,14 +32,58 @@ function component(width, height, color, x, y) {
     this.height = height;
     this.x = x;
     this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
     this.update = function() {
         ctx = gameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    this.move = function() {
+        this.x += this.speedX;
+        this.x += this.speedY;
+    }
 }
 
 function updateGameArea() {
     gameArea.clear();
-    gamePiece.update();
+    if (gameArea.key == "up") {
+        moveUp();
+    }
+    if (gameArea.key == "down") {
+        moveDown();
+    }
+    if (gameArea.key == "left") {
+        moveLeft();
+    }
+    if (gameArea.key == "right") {
+        moveRight();
+    }
+    player.update();
+    door.update();
+    player.newPos();
+    door.newPos();
+}
+
+function moveUp() {
+    player.speedY -= 1;
+}
+
+function moveDown() {
+    player.speedY += 1;
+}
+
+function moveLeft() {
+    player.speedX -= 1;
+}
+
+function moveRight() {
+    player.speedX += 1;
+}
+
+function stopMovement() {
+    player.speedX = 0;
+    door.speedX = 0;
+    player.speedY = 0;
+    door.speedY = 0;
 }
